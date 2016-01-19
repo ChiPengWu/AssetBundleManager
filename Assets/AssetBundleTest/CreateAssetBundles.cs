57135af4fb3ea8fed4a60b4888901065e8eb48a3
Peter Wu <peter@spe3d.co>
2016-01-19 11:21:35 +0800

    Importing AssetBundleManager and assets

---
+++ Assets/AssetBundleTest/CreateAssetBundles.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System.Collections;
+
+public class CreateAssetBundles : MonoBehaviour 
+{
+	static AssetBundleManifest assetBundleManifest;
+	static AssetBundle bundle;
+
+	#if UNITY_EDITOR
+	[MenuItem ("Assets/Build AssetBundles")]
+	static void BuildAllAssetBundles ()
+	{
+		assetBundleManifest = BuildPipeline.BuildAssetBundles ("Assets//StreamingAssets/bundle");
+		AssetDatabase.Refresh();
+		string[] allAssetBundles = assetBundleManifest.GetAllAssetBundles();
+		foreach(var assetBundle in allAssetBundles){
+			print("AssetBundleName: " + assetBundle);
+//			string[] allDependencies = assetBundleManifest.GetAllDependencies(assetBundle);
+//			foreach(var dependencie in allDependencies){
+//				print("DependAssetBundleName: " + dependencie);
+//			}
+//			string[] directDependencies = assetBundleManifest.GetDirectDependencies(assetBundle);
+//			foreach(var direct in directDependencies){
+//				print("DirectDependAssetBundleName: " + direct);
+//			}
+
+			string assetsBundlePath = Application.streamingAssetsPath + "/bundle/" + assetBundle;
+
+			if(bundle != null)
+				bundle.Unload(true);
+			bundle = AssetBundle.LoadFromFile(assetsBundlePath);
+
+			string[] allAssetNames = bundle.GetAllAssetNames();
+			foreach(var assetName in allAssetNames){
+				print("AssetName: " + assetName);
+//				string[] assetPath = AssetDatabase.GetAssetPathsFromAssetBundleAndAssetName(assetBundle, assetName);
+//				foreach(var aPath in assetPath){
+//					print("AssetPath: " + aPath);
+//					string[] dependencie = AssetDatabase.GetDependencies(aPath);
+//					foreach(var dep in dependencie){
+//						print("Dependencie: " + dep);
+//					}
+//				}
+			}
+		}
+
+	}
+	#endif
+
+}
---
+++ Assets/AssetBundleTest/LoadBundle.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadBundle : MonoBehaviour {
+
+	AssetBundle bundle;
+
+	// Use this for initialization
+	void Start () {
+		string assetsBundlePath = Application.streamingAssetsPath + "/bundle/";
+
+		if(bundle != null)
+			bundle.Unload(false);
+		bundle = AssetBundle.LoadFromFile(assetsBundlePath + "bundle");
+
+		string[] allAssetNames = bundle.GetAllAssetNames();
+		foreach(var assetName in allAssetNames){
+			print("AssetName: " + assetName);
+			AssetBundleManifest assetBundleManifest = bundle.LoadAsset(assetName) as AssetBundleManifest;
+			string[] allAssetBundles = assetBundleManifest.GetAllAssetBundles();
+			foreach(var assetBundle in allAssetBundles){
+				print("AssetBundleName: " + assetBundle);
+				bundle = AssetBundle.LoadFromFile(assetsBundlePath + assetBundle);
+				GameObject go = bundle.LoadAsset(assetBundle) as GameObject;
+				Instantiate(go);
+			}
+
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
